79cc87f2fbe58f26dad66f94cc9894f7ab8f8a87
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-08-24 12:25:37 +0900

    feat:MDNチュートリアル4章まで　公式チュートリアルもしたほうがよさそう

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable react-refresh/only-export-components */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App subject="Clarice" />
+    <App tasks={DATA}/>
   </StrictMode>,
 )
