745f7dfc55de7331e70fa110b04872961aa443ba
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-07 17:59:43 +0900

    未追跡ファイルをコミットさせないでください

--- cesium_jissou/src/App.tsx
+++ cesium_jissou/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-
+ 
 import { Camera } from './Camera'
+import { Clock } from './Clock'
+import { Lighting } from './Lighting'
 import { PlateauTileset } from './PlateauTileset'
 import { Viewer } from './Viewer'
-
+ 
 export const App: React.FC = () => {
-    return (
-        <Viewer>
-            <Camera />
-            <PlateauTileset path='bldg/13100_tokyo/13101_chiyoda-ku/notexture' />
-            <PlateauTileset path='bldg/13100_tokyo/13102_chuo-ku/notexture' />
-        </Viewer>
-    )
+  return (
+    <Viewer>
+      <Camera />
+      <Clock />
+      <Lighting />
+      <PlateauTileset path='bldg/13100_tokyo/13101_chiyoda-ku/notexture' />
+      <PlateauTileset path='bldg/13100_tokyo/13102_chuo-ku/notexture' />
+    </Viewer>
+  )
 }
