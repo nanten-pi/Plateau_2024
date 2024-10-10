7bcf6b945e6c303fa96696b9feece26c2fb10124
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-10 18:30:24 +0900

    feat:どうしてもcesiumが見つからないらしい

---
+++ cesium-app/app/page.tsx
@@ -0,0 +1,40 @@
+// app/page.tsx
+'use client';
+
+import { useEffect, useRef } from 'react';
+import * as Cesium from 'cesium';
+
+const accessToken = process.env.NEXT_PUBLIC_CESIUM_TOKEN;
+
+if (!accessToken) {
+  throw new Error('NEXT_PUBLIC_CESIUM_TOKEN is not defined');
+}
+
+Cesium.Ion.defaultAccessToken = accessToken;
+
+export default function Home() {
+  const cesiumContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (cesiumContainerRef.current) {
+      Cesium.createWorldTerrainAsync().then(terrainProvider => {
+        const viewer = new Cesium.Viewer(cesiumContainerRef.current as Element, {
+          terrainProvider: terrainProvider,
+        });
+
+        return () => {
+          viewer.destroy();
+        };
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <div
+        ref={cesiumContainerRef}
+        style={{ width: '100%', height: '100vh', position: 'absolute' }}
+      />
+    </div>
+  );
+}
