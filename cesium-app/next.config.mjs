52a28d5ed7faa35e806b7f6a9a39f4323d772843
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-10 22:52:08 +0900

    いまだに出来ません（copyを止めてみましたが...)

--- cesium-app/next.config.mjs
+++ cesium-app/next.config.mjs
@@ -1,4 +1,3 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
@@ -6,35 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
-  webpack: (config, { isServer }) => {
-    // Cesium の静的ファイルをコピーする
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: resolve(__dirname, 'node_modules/cesium/Build/Cesium'),
-            to: 'public/cesium',
-            force: true, // ファイルが存在していても強制的にコピー
-          },
-        ],
-      }),
-
-    );
-
-    // Cesium のモジュール解決のエイリアス設定
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       cesium: resolve(__dirname, 'node_modules/cesium/Source'),
     };
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        zlib: false,
-      };
-    }
-    config.cache = true;
     return config;
   },
 };
--- cesium-app/src/app/page.tsx
+++ cesium-app/src/app/page.tsx
@@ -1,42 +1,18 @@
-// app/page.tsx
 'use client';
 
 import { useEffect, useRef } from 'react';
-import * as Cesium from 'cesium';
+import { Viewer } from 'cesium';
 
-import 'cesium/Build/Cesium/Widgets/widgets.css';
-
-const accessToken = process.env.NEXT_PUBLIC_CESIUM_TOKEN;
-
-if (!accessToken) {
-  throw new Error('NEXT_PUBLIC_CESIUM_TOKEN is not defined');
-}
-
-Cesium.Ion.defaultAccessToken = accessToken;
-
-export default function Home() {
-  const cesiumContainerRef = useRef<HTMLDivElement>(null);
+export default function Page() {
+  const cesiumContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (cesiumContainerRef.current) {
-      Cesium.createWorldTerrainAsync().then(terrainProvider => {
-        const viewer = new Cesium.Viewer(cesiumContainerRef.current as Element, {
-          terrainProvider: terrainProvider,
-        });
+    const viewer = new Viewer(cesiumContainer.current!);
 
-        return () => {
-          viewer.destroy();
-        };
-      });
-    }
+    return () => {
+      viewer.destroy();
+    };
   }, []);
 
-  return (
-    <div>
-      <div
-        ref={cesiumContainerRef}
-        style={{ width: '100%', height: '100vh', position: 'absolute' }}
-      />
-    </div>
-  );
+  return <div ref={cesiumContainer} style={{ width: '100%', height: '100vh' }} />;
 }
