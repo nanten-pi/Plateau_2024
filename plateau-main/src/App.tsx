02220c217c08e47de11bb03da03ac6aeb631fa97
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-12 13:10:32 +0900

    feat:カメラを広島市に

--- plateau-main/src/App.tsx
+++ plateau-main/src/App.tsx
@@ -5,6 +5,7 @@ import { Clock } from './Clock'
 import { Lighting } from './Lighting'
 import { PlateauTileset } from './PlateauTileset'
 import { Viewer } from './Viewer'
+//上はPlateau公式の地図,下は地質調査総合センター図
 import { PlateauOrtho } from './PlateauOrtho'
 import { GeologicalSurveyData } from './GeologicalSurveyData'
 import { PlateauTerrain } from './PlateauTerrain'
@@ -16,7 +17,7 @@ export const App: React.FC = () => {
       <Clock />
       <Lighting />
       <PlateauTerrain />
-      <GeologicalSurveyData />
+      <PlateauOrtho />
       <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/e4/24967c-e7c8-4909-a187-aa7af98f7ae5/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34101_naka-ku_lod2_no_texture/' />
     </Viewer>
   )
