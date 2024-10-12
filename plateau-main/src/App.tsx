8df45b1e76da1d592e643dcc9d713a1d57a953f6
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-12 13:24:55 +0900

    feat:*このバージョンのコードは実行できません　旧方式では東京以外のデータがうまく扱えるか不明。Hiroshimaは使えません

--- plateau-main/src/App.tsx
+++ plateau-main/src/App.tsx
@@ -18,7 +18,7 @@ export const App: React.FC = () => {
       <Lighting />
       <PlateauTerrain />
       <PlateauOrtho />
-      <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/e4/24967c-e7c8-4909-a187-aa7af98f7ae5/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34101_naka-ku_lod2_no_texture/' />
+      <PlateauTileset path='bldg/340006_hiroshima/341002_hiroshima-shi/notexture' />
     </Viewer>
   )
 }
