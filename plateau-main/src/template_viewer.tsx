3de7e37a94fdd545eec71547d8615ccde2167a74
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-12 11:38:34 +0900

    feat:開発を容易にするためにテンプレートを追加

---
+++ plateau-main/src/template_viewer.tsx
@@ -0,0 +1,26 @@
+import { ViewerContext } from './Viewer';
+import { useEffect, useContext } from 'react';
+//importしたい機能を「,」の後に追記してください
+import { UrlTemplateImageryProvider } from 'cesium';
+
+export const template_viewer: React.FC = () => {
+    const viewer = useContext(ViewerContext);
+
+    useEffect(() => {
+        if (viewer?.isDestroyed() !== false) {
+            return;
+        }
+        //ここから先に追記
+        var positron = new UrlTemplateImageryProvider({
+            url: 'https://gbank.gsj.jp/seamless/v2/api/1.2/tiles/{z}/{y}/{x}.png',
+            credit: '地質調査総合センター'
+        });
+        //viewerに渡せます
+        viewer.scene.imageryLayers.addImageryProvider(positron);
+    }, [viewer]);
+
+    return null;
+};
+//最後はApp.tsxにモジュールを読み込ませてください
+//例
+//import {template_viewer} from './template_viewer'
