9812069b82c01620f5dfd24840d6e222dfe54998
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-12 21:21:35 +0900

    feat:ハザードマップデータのレイヤリング機能を追加(広島県限定) 多層連携はまだむずそう

--- plateau-main/src/App.tsx
+++ plateau-main/src/App.tsx
@@ -4,9 +4,10 @@ import { Camera } from './Camera'
 import { Clock } from './Clock'
 import { Lighting } from './Lighting'
 import { Viewer } from './Viewer'
-//上はPlateau公式の地図,下は地質調査総合センター図
+//1.Plateau公式の地図,2.地質調査総合センター図 3.ハザードマップ図
 import { PlateauOrtho } from './PlateauOrtho'
 import { GeologicalSurveyData } from './GeologicalSurveyData'
+import { HazardMapData } from './HazardMapData'
 import { PlateauTerrain } from './PlateauTerrain'
 import { PlateauModelLatest } from './PlateauModelLatest'
 export const App: React.FC = () => {
@@ -16,7 +17,7 @@ export const App: React.FC = () => {
       <Clock />
       <Lighting />
       <PlateauTerrain />
-      <PlateauOrtho />
+      <HazardMapData />
       <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/cb/7bac72-24c1-4901-b1f4-9373e2feb738/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34102_higashi-ku_lod2' />
       <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/5d/e5c519-682e-43fc-9bbb-744b8dd665ba/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34103_minami-ku_lod2' />
       <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/a6/2ab468-91d9-4f5b-bdb2-058037d6e257/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34105_asaminami-ku_lod1' />
---
+++ plateau-main/src/HazardMapData.tsx
@@ -0,0 +1,20 @@
+import { ViewerContext } from './Viewer';
+import { useEffect, useContext } from 'react';
+import { UrlTemplateImageryProvider } from 'cesium';
+
+export const HazardMapData: React.FC = () => {
+    const viewer = useContext(ViewerContext);
+
+    useEffect(() => {
+        if (viewer?.isDestroyed() !== false) {
+            return;
+        }
+        var positron = new UrlTemplateImageryProvider({
+            url: 'https://disaportaldata.gsi.go.jp/raster/01_flood_l2_shinsuishin_pref_data/34/{z}/{x}/{y}.png',
+            credit: '重ねるハザードマップ'
+        });
+        viewer.scene.imageryLayers.addImageryProvider(positron);
+    }, [viewer]);
+
+    return null;
+};
