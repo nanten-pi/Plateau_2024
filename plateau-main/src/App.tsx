40611ee110347a0a8ededda897c47948c753fba0
nAntEn_Pi <88582003+nanten-pi@users.noreply.github.com>
2024-10-12 13:43:11 +0900

    feat:モデルを3区分追加　テクスチャがあると重たいかも

--- plateau-main/src/App.tsx
+++ plateau-main/src/App.tsx
@@ -16,8 +16,11 @@ export const App: React.FC = () => {
       <Clock />
       <Lighting />
       <PlateauTerrain />
-      <GeologicalSurveyData />
+      <PlateauOrtho />
       <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/28/dda2f5-309c-4e2a-98f1-3961027ed4e5/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34101_naka-ku_lod2' />
+      <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/cb/7bac72-24c1-4901-b1f4-9373e2feb738/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34102_higashi-ku_lod2' />
+      <PlateauModelLatest path='https://assets.cms.plateau.reearth.io/assets/5d/e5c519-682e-43fc-9bbb-744b8dd665ba/34100_hirosima-shi_city_2022_citygml_3_op_bldg_3dtiles_34103_minami-ku_lod2' />
+
     </Viewer>
   )
 }
