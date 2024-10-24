a457e0c5dfbf8ddb175230c984e2d39b3348d38e
ragilispec <godof256@gmail.com>
2024-10-25 01:17:08 +0900

    feat:infoUIsakusei

--- plateau-main/src/Style.tsx
+++ plateau-main/src/Style.tsx
@@ -24,6 +24,11 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import {Viewer} from './Viewer';
 import { PlateauOrtho } from './PlateauOrtho';
@@ -39,12 +44,22 @@ import { XmlLoder } from './XmlLoder'
 import { KmlLoder } from './KmlLoder';
 import { GeologicalSurveyData } from './GeologicalSurveyData';
 import { Pointer } from './Pointer';
+import { TransitionProps } from '@mui/material/transitions';
  //Material UI をいんぽしてつかってる
 const drawerWidth: number = 240;
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
 }
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<unknown>;
+  },//この辺が情報入力画面の
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -100,6 +115,16 @@ function DashboardContent() {
   const [sort, setSort] = React.useState(''); //ここからプルダウンのやつです
   const handleChange = (event: SelectChangeEvent) => {
     setSort(event.target.value as string);
+  };
+
+  const [openinfo, setOpeninfo] = React.useState(false);//情報入力のはずだけどUIが何も映らなくなる
+
+  const handleClickOpen = () => {
+    setOpeninfo(true);
+  };
+
+  const handleClose = () => {
+    setOpeninfo(false);
   };
 
   return (
@@ -177,18 +202,60 @@ function DashboardContent() {
           </FormGroup>
           <Divider />
           <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          //value={sort}これは使いたいときどうぞ
-          label="Sort"
-          //onChange={handleChange}これは使いたいときどうぞ
-        >
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                //value={sort}これは使いたいときどうぞ
+                label="Sort"
+                //onChange={handleChange}これは使いたいときどうぞ
+              >
           <MenuItem value={0}>Layer1</MenuItem>
           <MenuItem value={1}>Layer2</MenuItem>
           <MenuItem value={2}>Layer3</MenuItem>
+          <React.Fragment>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              Open full-screen dialog
+            </Button>
+            <Dialog
+              fullScreen
+              open={openinfo}
+              onClose={handleClose}
+              TransitionComponent={Transition}
+            >
+              <AppBar sx={{ position: 'relative' }}>
+                <Toolbar>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleClose}
+                    aria-label="close"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                    Sound
+                  </Typography>
+                  <Button autoFocus color="inherit" onClick={handleClose}>
+                    save
+                  </Button>
+                </Toolbar>
+              </AppBar>
+              <List>
+                <ListItemButton>
+                  <ListItemText primary="Phone ringtone" secondary="Titania" />
+                </ListItemButton>
+                <Divider />
+                <ListItemButton>
+                  <ListItemText
+                    primary="Default notification ringtone"
+                    secondary="Tethys"
+                  />
+                </ListItemButton>
+              </List>
+            </Dialog>
+          </React.Fragment>
         </Select>
       </FormControl>
     </Box>
